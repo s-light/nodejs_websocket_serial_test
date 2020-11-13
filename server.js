cac5acd1fa995d4033f9352467ecaee73d727dc6
s-light <git@s-light.eu>
2020-11-14 00:58:40 +0100

    prepare

---
+++ server.js
@@ -0,0 +1,6 @@
+const SerialPort = require('serialport')
+const Readline = require('@serialport/parser-readline')
+const port = new SerialPort('/dev/tty-usbserial1')
+
+const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
+parser.on('data', console.log)
